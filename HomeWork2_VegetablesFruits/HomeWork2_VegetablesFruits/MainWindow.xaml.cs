de0e194cb3945461641d5f53d6206c794a637887
deus-tll <millerartemka14@gmail.com>
2023-02-13 23:39:41 +0200

    update

---
+++ HomeWork2_VegetablesFruits/HomeWork2_VegetablesFruits/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeWork2_VegetablesFruits
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private readonly VegetablesFruitsDB DB = new();
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Btn_GetProviders_Click(object sender, RoutedEventArgs e)
+		{
+			Cmb_Providers.Items.Clear();
+			Cmb_Providers.ItemsSource = DB.GetProvidersList();
+		}
+
+		private void Cmb_Providers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			TxtBox_ConnectionString.Text =  DB.ChangingProvider(Cmb_Providers.SelectedItem.ToString());
+		}
+
+		private void Btn_Disconnect_Click(object sender, RoutedEventArgs e)
+		{
+			DB.DisconnectBase();
+		}
+
+		private void Btn_Connect_Click(object sender, RoutedEventArgs e)
+		{
+			DB.ConnectBase();
+		}
+	}
+}
---
+++ HomeWork2_VegetablesFruits/HomeWork2_VegetablesFruits/VegetablesFruitsDB.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace HomeWork2_VegetablesFruits
+{
+	internal class VegetablesFruitsDB
+	{
+		private DbConnection? _connection;
+		private DbProviderFactory? _factory;
+		private string? _connectionString = "";
+		private DbDataAdapter? _adapter;
+
+		public async void ConnectBase()
+		{
+			_connection?.Dispose();
+
+			try
+			{
+				_connection = _factory?.CreateConnection();
+
+				if (_connection is not null)
+				{
+					_connection.ConnectionString = _connectionString;
+					await _connection.OpenAsync();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Connection Failed!\n\n" + ex.Message);
+				return;
+			}
+		}
+
+		public async void DisconnectBase()
+		{
+			if (_connection is not null)
+			{
+				await _connection.DisposeAsync();
+				MessageBox.Show("Connection Interrupted!");
+			}
+		}
+
+		public async Task<DataTable> GetTableByQuery(string query)
+		{
+			_adapter = _factory?.CreateDataAdapter();
+			if (_adapter is not null && _connection is not null)
+			{
+				_adapter.SelectCommand = _connection.CreateCommand();
+				_adapter.SelectCommand.CommandText = query;
+				await Task.Run<DataTable>(() =>
+				{
+					DataTable t = new();
+					_adapter.Fill(t);
+					return t;
+				});
+			}
+
+			return new DataTable();
+		}
+
+		public List<string> GetProvidersList()
+		{
+			List<string> providers = new();
+
+			DataTable table = DbProviderFactories.GetFactoryClasses();
+
+			string? tmp;
+			foreach (DataRow provider in table.Rows)
+			{
+				tmp = provider["InvariantName"].ToString();
+
+				if (tmp is not null)
+					providers.Add(tmp);
+			}
+
+			return providers;
+		}
+
+		public string? ChangingProvider(string? provider)
+		{
+			if (provider is not null)
+			{
+				_factory = DbProviderFactories.GetFactory(provider);
+				_connectionString = GetConnectionString(provider);
+
+				return _connectionString;
+			}
+			return null;
+		}
+
+		private string? GetConnectionString(string provider) 
+		{
+			string? result = null;
+			ConnectionStringSettingsCollection settingsCollection = ConfigurationManager.ConnectionStrings;
+			if (settingsCollection is not null)
+			{
+				foreach (ConnectionStringSettings item in settingsCollection)
+				{
+					if (item.ProviderName == provider)
+					{
+						result = item.ConnectionString;
+						break;
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
