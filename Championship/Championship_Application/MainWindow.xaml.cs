d82d8d080b22e60d6a8fdb62a661b4a94a854604
deus-tll <millerartemka14@gmail.com>
2023-02-20 23:10:03 +0200

    update

--- Championship/Championship_Application/MainWindow.xaml.cs
+++ Championship/Championship_Application/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Championship_Application.Сlasses;
 using Championship_Models.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
-
+using System.Windows.Controls;
 
 namespace Championship_Application
 {
@@ -24,6 +27,8 @@ namespace Championship_Application
 
 		private void FillDataGridOnEditingPage(Table table)
 		{
+			Dgv_RecordsListEditing.ItemsSource = null;
+
 			switch (table)
 			{
 				case Table.Teams: Dgv_RecordsListEditing.ItemsSource = _app.GetTeams(); break;
@@ -31,10 +36,122 @@ namespace Championship_Application
 		}
 
 
+		private bool CheckTeamFields()
+		{
+			if (string.IsNullOrEmpty(TextBox_TeamName.Text) ||
+				string.IsNullOrEmpty(TextBox_TeamCity.Text) ||
+				string.IsNullOrEmpty(TextBox_NumberOfWins.Text) ||
+				string.IsNullOrEmpty(TextBox_NumberOfDefeats.Text) ||
+				string.IsNullOrEmpty(TextBox_NumberOfDraws.Text) ||
+				string.IsNullOrEmpty(TextBox_NumberOfGoalsScored.Text) ||
+				string.IsNullOrEmpty(TextBox_NumberOfMissedGoals.Text))
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+
+		private async void AddTeam()
+		{
+			if (!CheckTeamFields())
+			{
+				MessageBox.Show("Всі поля мають бути заповнені!");
+				return;
+			}
+
+			if (_app.IsTeamAlreadyInBase(TextBox_TeamName.Text, TextBox_TeamCity.Text))
+			{
+				MessageBox.Show("Така команда вже існує!");
+				return;
+			}
+			
+			_app.AddTeam(TextBox_TeamName.Text,
+						 TextBox_TeamCity.Text, 
+						 Convert.ToInt32(TextBox_NumberOfWins.Text), 
+						 Convert.ToInt32(TextBox_NumberOfDefeats.Text), 
+						 Convert.ToInt32(TextBox_NumberOfDraws.Text), 
+						 Convert.ToInt32(TextBox_NumberOfGoalsScored.Text),
+						 Convert.ToInt32(TextBox_NumberOfMissedGoals.Text));
+
+			await _app.SaveAllChanges(Btn_AddItem);
+			FillDataGridOnEditingPage(Table.Teams);
+		}
+
+
+		private async void UpdateTeamInfo()
+		{
+			if (!CheckTeamFields())
+			{
+				MessageBox.Show("Всі поля мають бути заповнені!");
+				return;
+			}
+
+			if (_app.IsTeamAlreadyInBase(TextBox_TeamName.Text, TextBox_TeamCity.Text))
+			{
+				MessageBox.Show("Така команда вже існує!");
+				return;
+			}
+
+			Team? team = Dgv_RecordsListEditing.SelectedItem as Team;
+
+			if (team is null)
+				return;
+
+			team.Name = TextBox_TeamName.Text;
+			team.City = TextBox_TeamCity.Text;
+			team.NumberOfWins = Convert.ToInt32(TextBox_NumberOfWins.Text);
+			team.NumberOfDefeats = Convert.ToInt32(TextBox_NumberOfDefeats.Text);
+			team.NumberOfDraws = Convert.ToInt32(TextBox_NumberOfDraws.Text);
+			team.NumberOfGoalsScored = Convert.ToInt32(TextBox_NumberOfGoalsScored.Text);
+			team.NumberOfMissedGoals = Convert.ToInt32(TextBox_NumberOfMissedGoals.Text);
+
+			await _app.SaveAllChanges(Btn_SaveChanges);
+			FillDataGridOnEditingPage(Table.Teams);
+		}
+
+
+		private async void RemoveTeam()
+		{
+			if (string.IsNullOrEmpty(TextBox_TeamName.Text) || string.IsNullOrEmpty(TextBox_TeamCity.Text))
+				return;
+
+			Team? team = _app.GetTeamByNameAndCity(TextBox_TeamName.Text, TextBox_TeamCity.Text);
+
+			if (team is null) return;
+
+			MessageBoxResult result = MessageBox.Show("Видалити команду?", "Підтвердити...", MessageBoxButton.YesNo, MessageBoxImage.Question);
+			if (result != MessageBoxResult.Yes) return;
+
+			_app.RemoveTeam(team);
+
+			await _app.SaveAllChanges(Btn_RemoveItem);
+			FillDataGridOnEditingPage(Table.Teams);
+		}
+
+
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			Cmb_TablesList.ItemsSource = Enum.GetValues(typeof(Table)).Cast<Table>();
 			Cmb_TablesList.SelectedItem = Cmb_TablesList.Items[0];
+		}
+
+
+		private void Dgv_RecordsListEditing_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			Team? team = Dgv_RecordsListEditing.SelectedItem as Team;
+
+			if (team is null)
+				return;
+
+			TextBox_TeamName.Text = team.Name;
+			TextBox_TeamCity.Text = team.City;
+			TextBox_NumberOfWins.Text = team.NumberOfWins.ToString();
+			TextBox_NumberOfDefeats.Text = team.NumberOfDefeats.ToString();
+			TextBox_NumberOfDraws.Text = team.NumberOfDraws.ToString();
+			TextBox_NumberOfGoalsScored.Text = team.NumberOfGoalsScored.ToString();
+			TextBox_NumberOfMissedGoals.Text = team.NumberOfMissedGoals.ToString();
 		}
 
 
@@ -46,9 +163,93 @@ namespace Championship_Application
 		}
 
 
+		private void Btn_AddItem_Click(object sender, RoutedEventArgs e)
+		{
+			switch ((Table)Cmb_TablesList.SelectedValue)
+			{
+				case Table.Teams: AddTeam(); break;
+			}
+		}
+
+
+		private void Btn_RemoveItem_Click(object sender, RoutedEventArgs e)
+		{
+			switch ((Table)Cmb_TablesList.SelectedValue)
+			{
+				case Table.Teams: RemoveTeam(); break;
+			}
+		}
+
+
 		private void Btn_SaveChanges_Click(object sender, RoutedEventArgs e)
 		{
-			_app.SaveChanges();
+			switch ((Table)Cmb_TablesList.SelectedValue)
+			{
+				case Table.Teams: UpdateTeamInfo(); break;
+			}
+		}
+
+
+		private void Btn_FindTeamByName_Click(object sender, RoutedEventArgs e)
+		{
+			if (TextBox_FindTeamByName.Text is not "")
+			{
+				Dgv_RecordsListQueries.ItemsSource = _app.FindTeamByName(TextBox_FindTeamByName.Text);
+			}
+		}
+
+
+		private void Btn_FindTeamsByCity_Click(object sender, RoutedEventArgs e)
+		{
+			if (TextBox_FindTeamsByCity.Text is not "")
+			{
+				Dgv_RecordsListQueries.ItemsSource = _app.FindTeamsByCity(TextBox_FindTeamsByCity.Text);
+			}
+		}
+
+
+		private void Btn_FindInfoByNameAndCity_Click(object sender, RoutedEventArgs e)
+		{
+			if (TextBox_FindInfoByTeam.Text is not "" && TextBox_FindInfoByCity.Text is not "")
+			{
+				Dgv_RecordsListQueries.ItemsSource = _app.FindInfoByNameAndCity(TextBox_FindInfoByTeam.Text, TextBox_FindInfoByCity.Text);
+			}
+		}
+
+
+		private void Btn_GetTeamWithMostWins_Click(object sender, RoutedEventArgs e)
+		{
+			Dgv_RecordsListQueries.ItemsSource = _app.GetTeamWithMostWins();
+		}
+
+
+		private void Btn_GetTeamWithMostLosses_Click(object sender, RoutedEventArgs e)
+		{
+			Dgv_RecordsListQueries.ItemsSource = _app.GetTeamWithMostLosses();
+		}
+
+
+		private void Btn_GetTeamWithMostDraws_Click(object sender, RoutedEventArgs e)
+		{
+			Dgv_RecordsListQueries.ItemsSource = _app.GetTeamWithMostDraws();
+		}
+
+
+		private void Btn_GetTeamWithMostGoalsScored_Click(object sender, RoutedEventArgs e)
+		{
+			Dgv_RecordsListQueries.ItemsSource = _app.GetTeamWithMostGoalsScored();
+		}
+
+
+		private void Btn_GetTeamWithMostMissedGoals_Click(object sender, RoutedEventArgs e)
+		{
+			Dgv_RecordsListQueries.ItemsSource = _app.GetTeamWithMostMissedGoals();
+		}
+
+
+		private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+		{
+			e.Handled = !int.TryParse(e.Text, out _);
 		}
 	}
 }
--- Championship/Championship_Application/Сlasses/Championship.cs
+++ Championship/Championship_Application/Сlasses/Championship.cs
@@ -2,11 +2,13 @@
 using Championship_Models.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Championship_Application.Сlasses
 {
@@ -31,9 +33,111 @@ namespace Championship_Application.Сlasses
 		}
 
 
-		public void SaveChanges()
+		public void AddTeam(string Name, string City, int NumberOfWins, int NumberOfDefeats, int NumberOfDraws, int NumberOfGoalsScored, int NumberOfMissedGoals)
 		{
+			Team team = new()
+			{
+				Name = Name,
+				City = City,
+				NumberOfWins = NumberOfWins,
+				NumberOfDefeats = NumberOfDefeats,
+				NumberOfDraws = NumberOfDraws,
+				NumberOfGoalsScored = NumberOfGoalsScored,
+				NumberOfMissedGoals = NumberOfMissedGoals
+			};
+
+			DB.Teams.Add(team);
 			DB.SaveChanges();
+		}
+
+
+		public void RemoveTeam(Team team)
+		{
+			DB.Teams.Remove(team);
+		}
+
+
+		public async Task<int> SaveAllChanges(Button sender)
+		{
+			sender.IsEnabled = false;
+			try
+			{
+				return await DB.SaveChangesAsync();
+			}
+			finally
+			{
+				sender.IsEnabled = true;
+			}
+		}
+
+
+		public bool IsTeamAlreadyInBase(string name, string city)
+		{
+			DB.Teams.Load();
+			return DB.Teams.Any(t => t.Name == name && t.City == city);
+		}
+
+
+		public Team? GetTeamByNameAndCity(string name, string city)
+		{
+			DB.Teams.Load();
+			return DB.Teams.FirstOrDefault(t => t.Name == name && t.City == city);
+		}
+
+
+		public IEnumerable FindTeamByName(string name)
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().Where(t => t.Name == name);
+		}
+
+
+		public IEnumerable FindTeamsByCity(string city)
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().Where(t => t.City == city);
+		}
+
+
+		public IEnumerable FindInfoByNameAndCity(string name, string city)
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().Where(t => t.Name == name && t.City == city);
+		}
+
+
+		public IEnumerable GetTeamWithMostWins()
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().OrderByDescending(t => t.NumberOfWins).Take(1);
+		}
+
+
+		public IEnumerable GetTeamWithMostLosses()
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().OrderByDescending(t => t.NumberOfDefeats).Take(1);
+		}
+
+
+		public IEnumerable GetTeamWithMostDraws()
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().OrderByDescending(t => t.NumberOfDraws).Take(1);
+		}
+
+
+		public IEnumerable GetTeamWithMostGoalsScored()
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().OrderByDescending(t => t.NumberOfGoalsScored).Take(1);
+		}
+
+
+		public IEnumerable GetTeamWithMostMissedGoals()
+		{
+			DB.Teams.Load();
+			return DB.Teams.ToList().OrderByDescending(t => t.NumberOfMissedGoals).Take(1);
 		}
 	}
 }
